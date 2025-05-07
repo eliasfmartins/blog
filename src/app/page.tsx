e700a0987438661676c221e7a20950231155008c
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-05-07 09:14:51 -0300

    feat: layout e metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/components/container";
-import { Header } from "@/components/Header";
 import { PostFeatured } from "@/components/PostFeatured";
 import { PostsLists } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
@@ -8,14 +6,13 @@ import { Suspense } from "react";
 export default async function Home() {
 
   return (
-    <Container>
-      <Header />
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
       <Suspense fallback={<SpinLoader />}>
         <PostsLists />
       </Suspense>
-    </Container>
+    </>
   );
 }
