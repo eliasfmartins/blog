c432ca1bb8afc50f5425a3839ab1e42bf6038ba0
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-05-09 09:46:29 -0300

    feat: MetaData dinamico

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,13 @@ import clsx from "clsx";
 
 export default function NotFound() {
   return (
-    <div className={clsx("min-h-[320px] bg-slate-900 text-slate-100", 'mb-16 p-8 rounded-xl', 'flex items-center justify-center', 'text-center')}>
-      <div>
-        <h1 className="text-7xl/tight mb-5 font-extrabold ">404</h1>
-        <p>Erro 404 - a Página que você está tentando acessar não exisste neste site.</p>
-      </div>
-    </div>
+    <>
+      <title>404 - Página não encontrada</title>
+      <div className={clsx("min-h-[320px] bg-slate-900 text-slate-100", 'mb-16 p-8 rounded-xl', 'flex items-center justify-center', 'text-center')}>
+        <div>
+          <h1 className="text-7xl/tight mb-5 font-extrabold ">404</h1>
+          <p>Erro 404 - a Página que você está tentando acessar não exisste neste site.</p>
+        </div>
+      </div></>
   );
 }
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,19 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type PostSlugProps = {
   params: Promise<{ slug: string }>
 }
 
+export async function generateMetadata({ params }: PostSlugProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug)
+  return {
+    title: `Post - ${post.title}`,
+    description: `Post - ${post.excerpt}`,
+  }
+}
 
 export default async function SlugPage({ params }: PostSlugProps) {
   const { slug } = await params;
