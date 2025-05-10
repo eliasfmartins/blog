2ca426b616deac69add751463bed7b07ef260f82
eliasfmartins <eliasmartinsdev8@gmail.com>
2025-05-10 11:56:44 -0300

    style: estilizando markdown com taiwind e plugins

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import ReactMarkdown from 'react-markdown'
 import rehypeSanitize from 'rehype-sanitize'
 import remarkGfm from 'remark-gfm'
@@ -8,8 +9,10 @@ type SafeMarkDownProps = {
 
 export function SafeMarkDown({ markdown }: SafeMarkDownProps) {
   return (
-    <div>
-      <ReactMarkdown rehypePlugins={[rehypeSanitize]} remarkPlugins={[remarkGfm]}>
+    <div className={clsx('prose prose-slate', 'w-full', 'max-w-full', 'dark:prose-invert', ' prose-a:text-blue-500 prose-a:hover:text-blue-700 prose-a:transition prose-a:no-underline prose-img:mx-auto md:prose-lg')}>
+      <ReactMarkdown
+        rehypePlugins={[rehypeSanitize]}
+        remarkPlugins={[remarkGfm]}>
         {markdown}
       </ReactMarkdown>
     </div>
